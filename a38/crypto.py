eefa097aab7b57e9fbd18b377dd1529b84d34085
Enrico Zini <enrico@enricozini.org>
2019-02-26 17:35:15 +0100

    Implemented basic signature verification using openssl via subprocess

--- a38/crypto.py
+++ a38/crypto.py
@@ -3,8 +3,21 @@ from asn1crypto.cms import ContentInfo
 import io
 import base64
 import binascii
+import subprocess
 import xml.etree.ElementTree as ET
 from . import fattura as a38
+
+
+class SignatureVerificationError(Exception):
+    pass
+
+
+class InvalidSignatureError(SignatureVerificationError):
+    pass
+
+
+class SignerCertificateError(SignatureVerificationError):
+    pass
 
 
 class P7M:
@@ -64,3 +77,30 @@ class P7M:
         data = io.BytesIO(self.get_payload())
         tree = ET.parse(data)
         return a38.auto_from_etree(tree.getroot())
+
+    def verify_signature(self, certdir):
+        """
+        Verify the signature on the file
+        """
+        res = subprocess.run([
+            "openssl", "cms", "-verify", "-inform", "DER", "-CApath", certdir, "-noout"],
+            input=self.data,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE)
+
+        # From openssl cms manpage:
+        # 0   The operation was completely successfully.
+        # 1   An error occurred parsing the command options.
+        # 2   One of the input files could not be read.
+        # 3   An error occurred creating the CMS file or when reading the MIME message.
+        # 4   An error occurred decrypting or verifying the message.
+        # 5   The message was verified correctly but an error occurred writing out the signers certificates.
+
+        if res.returncode == 0:
+            pass
+        elif res.returncode == 4:
+            raise InvalidSignatureError(res.stderr)
+        elif res.returncode == 5:
+            raise SignerCertificateError(res.stderr)
+        else:
+            raise RuntimeError(res.stderr)
--- tests/test_p7m.py
+++ tests/test_p7m.py
@@ -1,8 +1,53 @@
 from unittest import TestCase
-from a38.crypto import P7M
+import tempfile
+from contextlib import contextmanager
+import os
+from a38.crypto import P7M, InvalidSignatureError, SignerCertificateError
+
+CA_CERT = """
+-----BEGIN CERTIFICATE-----
+MIIFJjCCBA6gAwIBAgIBATANBgkqhkiG9w0BAQsFADCBhTELMAkGA1UEBhMCSVQx
+FTATBgNVBAoMDElORk9DRVJUIFNQQTEiMCAGA1UECwwZQ2VydGlmaWNhdG9yZSBB
+Y2NyZWRpdGF0bzEUMBIGA1UEBRMLMDc5NDUyMTEwMDYxJTAjBgNVBAMMHEluZm9D
+ZXJ0IEZpcm1hIFF1YWxpZmljYXRhIDIwHhcNMTMwNDE5MTQyNjE1WhcNMjkwNDE5
+MTUyNjE1WjCBhTELMAkGA1UEBhMCSVQxFTATBgNVBAoMDElORk9DRVJUIFNQQTEi
+MCAGA1UECwwZQ2VydGlmaWNhdG9yZSBBY2NyZWRpdGF0bzEUMBIGA1UEBRMLMDc5
+NDUyMTEwMDYxJTAjBgNVBAMMHEluZm9DZXJ0IEZpcm1hIFF1YWxpZmljYXRhIDIw
+ggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDFoW5eA0k3AcU+/v2uKclE
+hGrxXlqOUptAQJLSjysP7IaKKtGxIeX8HNavxRaDkLkQNElql+t4GgIPyJk4lzHb
+H72c1Ls2SH06X7uCo5iGRH3+FU1ScbcrzviAPB+yeqUZ1cKkGyyGQ1wBsorxpREU
+eajkW2wsDiY/DYyeTG1I3ECOk/2sZW1U/xGWeVIlNCg9lkqrjvwu6swKVg3LPRiD
+L4Cqzsh4w9VZzJeDvKfer6lp/fRRduY5fSajtttgCoERrw0hZH/PkkmDCcnbLSjx
+59Knu3jHip5prgGVU29MKANf573VZAAZfau/lAxf1K91DEXxtPWknEUULt3beGef
+AgMBAAGjggGdMIIBmTAPBgNVHRMBAf8EBTADAQH/MFgGA1UdIARRME8wTQYEVR0g
+ADBFMEMGCCsGAQUFBwIBFjdodHRwOi8vd3d3LmZpcm1hLmluZm9jZXJ0Lml0L2Rv
+Y3VtZW50YXppb25lL21hbnVhbGkucGhwMCUGA1UdEgQeMByBGmZpcm1hLmRpZ2l0
+YWxlQGluZm9jZXJ0Lml0MIHVBgNVHR8Egc0wgcowgceggcSggcGGKmh0dHA6Ly9j
+cmwuaW5mb2NlcnQuaXQvY3Jscy9maXJtYTIvQVJMLmNybIaBkmxkYXA6Ly9sZGFw
+LmluZm9jZXJ0Lml0L2NuJTNESW5mb0NlcnQlMjBGaXJtYSUyMFF1YWxpZmljYXRh
+JTIwMixvdSUzRENlcnRpZmljYXRvcmUlMjBBY2NyZWRpdGF0byxvJTNESU5GT0NF
+UlQlMjBTUEEsYyUzRElUP2F1dGhvcml0eVJldm9jYXRpb25MaXN0MA4GA1UdDwEB
+/wQEAwIBBjAdBgNVHQ4EFgQUk90h/APQFQpyraPM1ZoJnTiLnekwDQYJKoZIhvcN
+AQELBQADggEBAJYdIAO8JCHr9dTT/kpy5AZpgo8XoIQW/q9tNQPwZkdd/bAfgLib
+olvbk7ZTsiVlVv35Bb9rhM58SKP1Xa9c26Cf8y4zhoplVbhfKRGVCLj1u1EXdPhC
+UQb8WWcM0AyLOXj3qhbMh77UL0K9eaRrwTAENbl43Jy65HPHubNnk9U9wIUUtLgR
+Hl5Oog1ZUSV5oLEkeSwzHyk5ZQnv24BzU9UXJ/amAt2ff1Krr3/PsY4Juwgtpg1N
+qq8tid5L+lN7qJ8xXfxMuUX2aWkWftCBL8H75U7NnYm/Zx6XyRaULFzCDw0RBSHa
+WGPH+t5X7ZMMERXn8Z/2LTYWuj9w1+WeieY=
+-----END CERTIFICATE-----
+"""
+
+CA_CERT_HASH = "af603d58.0"
 
 
 class TestAnagrafica(TestCase):
+    @contextmanager
+    def capath(self):
+        with tempfile.TemporaryDirectory() as td:
+            with open(os.path.join(td, CA_CERT_HASH), "wt") as fd:
+                fd.write(CA_CERT)
+            yield td
+
     def test_load(self):
         p7m = P7M("tests/data/test.txt.p7m")
         data = p7m.get_payload()
@@ -11,3 +56,32 @@ class TestAnagrafica(TestCase):
                 "This is only a test payload.\n"
                 "\n"
                 "Questo è solo un payload di test.\n".encode("utf8"))
+
+    def test_verify(self):
+        p7m = P7M("tests/data/test.txt.p7m")
+        with self.capath() as capath:
+            p7m.verify_signature(capath)
+
+    def test_verify_corrupted_random(self):
+        p7m = P7M("tests/data/test.txt.p7m")
+        data_mid = len(p7m.data) // 2
+        p7m.data = p7m.data[:data_mid] + bytes([p7m.data[data_mid] + 1]) + p7m.data[data_mid + 1:]
+        with self.capath() as capath:
+            with self.assertRaises(InvalidSignatureError):
+                p7m.verify_signature(capath)
+
+    def test_verify_corrupted_payload(self):
+        p7m = P7M("tests/data/test.txt.p7m")
+        signed_data = p7m.get_signed_data()
+        encap_content_info = signed_data["encap_content_info"]
+        encap_content_info["content"] = b"All your base are belong to us"
+        p7m.data = p7m.content_info.dump()
+        with self.capath() as capath:
+            with self.assertRaisesRegexp(InvalidSignatureError, r"routines:CMS_verify:content verify error"):
+                p7m.verify_signature(capath)
+
+    def test_verify_noca(self):
+        p7m = P7M("tests/data/test.txt.p7m")
+        with tempfile.TemporaryDirectory() as capath:
+            with self.assertRaisesRegexp(InvalidSignatureError, r"Verify error:unable to get local issuer certificate"):
+                p7m.verify_signature(capath)
