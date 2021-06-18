98b9a30998a86b0be4f70048406a45c3a297f5e1
ralcini <roberto.alcini@gmail.com>
2021-06-18 22:26:00 +0200

    Fix for issue #6

--- a38/render.py
+++ a38/render.py
@@ -33,7 +33,13 @@ if HAVE_LXML:
             with tempfile.NamedTemporaryFile("wb", suffix=".html") as fd:
                 html.write(fd)
                 fd.flush()
-                res = subprocess.run([wkhtmltopdf, fd.name, output_file], stdin=subprocess.DEVNULL, capture_output=True)
+                
+                cmdLine = [wkhtmltopdf, fd.name, output_file]
+                verifyLocalAccessToFileOption = subprocess.run([wkhtmltopdf], stdin=subprocess.DEVNULL, text=True, capture_output=True)
+                if "--enable-local-file-access" in verifyLocalAccessToFileOption.stdout:
+                    cmdLine.insert(1, "--enable-local-file-access")
+
+                res = subprocess.run(cmdLine, stdin=subprocess.DEVNULL, capture_output=True)
                 if res.returncode != 0:
                     raise RuntimeError("%s exited with error %d: stderr: %s", self.wkhtmltopdf, res.returncode, res.stderr)
             if output_file == "-":
